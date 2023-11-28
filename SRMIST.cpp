6e0cdbd186be6ef3552e0b415193c257564bc5ee
JUSTMEETPATEL <justmeetpatel@gmail.com>
2023-11-28 23:14:32 +0530

    Commiting again

--- SRMIST.cpp
+++ SRMIST.cpp
@@ -1,4 +1,4 @@
-// This program contians all the knowledge I have about OOPS and C++
+//This program contians all the knowledge I have about OOPS and C++
 
 #include <iostream>
 using namespace std;
@@ -23,166 +23,148 @@ Inheritance Tree
                                                                     _________________________________
                                                                     ↓       ↓       ↓       ↓       ↓
                                                                    CS      BDA     GT      IT    BIOTECH
-
+                                                                                                                                   
 
 */
 
-// Base Class SRMIST
+//Base Class SRMIST 
 
-class SRMIST
-{
-public:
-    string Name, Designation, FatherName, MotherName;
-    int age;
+class SRMIST{
+    public:
+        string Name,Designation,FatherName,MotherName;
+        int age;
 
-    SRMIST()
-    {
-        cout << "Enter Your Name :" << endl;
-        cin >> Name;
-        cout << "Enter Your Age :" << endl;
-        cin >> age;
-        cout << "Enter Your Father's Name :" << endl;
-        cin >> FatherName;
-        cout << "Enter Your Mother's Name :" << endl;
-        cin >> MotherName;
-    }
+        SRMIST()
+        {
+            cout<<"Enter Your Name :"<<endl;
+            cin>>Name;
+            cout<<"Enter Your Age :"<<endl;
+            cin>>age;
+            cout<<"Enter Your Father's Name :"<<endl;
+            cin>>FatherName;
+            cout<<"Enter Your Mother's Name :"<<endl;
+            cin>>MotherName;
+        }
+
 };
 
-// Derived Class from Base Class SRMIST - 1: Students
+//Derived Class from Base Class SRMIST - 1: Students
 
-class Students : public SRMIST
-{
-public:
-    Students()
-    {
-        Designation = "Student";
-    }
+class Students : public SRMIST {
+    public:
+        Students(){
+            Designation = "Student";
+            }
 };
 
-// Derived Class from Base Class SRMIST - 2: Teachers
+//Derived Class from Base Class SRMIST - 2: Teachers
 
-class Teachers : public SRMIST
-{
-public:
-    Teachers()
-    {
-        Designation = "Teacher";
-    }
+class Teachers : public SRMIST{
+    public:
+        Teachers(){
+            Designation = "Teacher";
+        }
 };
 
-// Derived Class from Base Class SRMIST - 3: Admin
+//Derived Class from Base Class SRMIST - 3: Admin
 
-class Admin : public SRMIST
-{
-    Admin()
-    {
+class Admin : public SRMIST{
+    Admin(){
         Designation = "Admin";
-    }
+        }
 };
 
 // Hybrid Class of Class Students and Teachers named BTECH
 
-class BTECH : public Students, public Teachers
-{
-public:
-    int year;
-    string sub1, sub2, sub3, sub4, sub5, sub6;
-    string branch;
-    BTECH()
-    {
-        cout << "Enter the year you are currenty studying :" << endl;
-        cin >> year;
-        cout << "Enter Subject-1 :" << endl;
-        cin >> sub1;
-        cout << "Enter Subject-2 :" << endl;
-        cin >> sub2;
-        cout << "Enter Subject-3 :" << endl;
-        cin >> sub3;
-        cout << "Enter Subject-4 :" << endl;
-        cin >> sub4;
-        cout << "Enter Subject-5 :" << endl;
-        cin >> sub5;
-        cout << "Enter Subject-6 :" << endl;
-        cin >> sub6;
-    }
+class BTECH : public Students, public Teachers{
+    public:
+        int year;
+        string sub1,sub2,sub3,sub4,sub5,sub6;
+        string branch;
+        BTECH()
+        {
+            cout<<"Enter the year you are currenty studying :"<<endl;
+            cin>>year;
+            cout<<"Enter Subject-1 :"<<endl;
+            cin>>sub1;
+            cout<<"Enter Subject-2 :"<<endl;
+            cin>>sub2;
+            cout<<"Enter Subject-3 :"<<endl;
+            cin>>sub3;
+            cout<<"Enter Subject-4 :"<<endl;
+            cin>>sub4;
+            cout<<"Enter Subject-5 :"<<endl;
+            cin>>sub5;
+            cout<<"Enter Subject-6 :"<<endl;
+            cin>>sub6;
+        }
 };
 
 // Deriving Class by taking hybrid class BTECH as Base Class named CSE
 
-class CSE : public BTECH
-{
-public:
-    CSE()
-    {
-        branch = "CSE CORE";
-    }
+class CSE : public BTECH{
+    public:
+        CSE(){
+            branch = "CSE CORE";
+        }
 };
 
 // Deriving an another Class by taking hybrid class BTECH as Base Class named CSE_Spec
 
-class CSE_Spec : public BTECH
-{
-public:
-    CSE_Spec()
-    {
-        branch = " ";
-    }
+class CSE_Spec : public BTECH{
+    public:
+        CSE_Spec(){
+            branch = " ";
+        }
 };
 
 // Derving Class CS by taking CSE_Spec as Base class
 
-class CS : public CSE_Spec
-{
-public:
-    CS()
-    {
-        branch = "CSE Cyber Security";
-    }
+class CS : public CSE_Spec{
+    public:
+        CS(){
+            branch = "CSE Cyber Security";
+        }
 };
 
 // Derving an antother Class BDA by taking CSE_Spec as Base class
 
-class BDA : public CSE_Spec
-{
+class BDA : public CSE_Spec{
 public:
-    BDA()
-    {
+    BDA() {
         branch = "CSE Big Data Analysis";
     }
-};
+}; 
 
 // Deriving another Class GT by taking CSE_Spec as Base class
 
-class GT : public CSE_Spec
-{
+class GT : public CSE_Spec{
 public:
-    GT()
-    {
+    GT() {
         branch = "CSE Gaming Technology";
     }
 };
 
 // Deriving another Class IT by taking CSE_Spec as Base class
 
-class IT : public CSE_Spec
-{
+class IT : public CSE_Spec{
 public:
-    IT()
-    {
+    IT() {
         branch = "CSE Information Technology";
     }
-};
+};  
 
 // Deriving another Class BIOTECH by taking CSE_Spec as Base class
 
-class BIOTECH : public CSE_Spec
-{
+class BIOTECH : public CSE_Spec{
 public:
-    BIOTECH()
-    {
+    BIOTECH() {
         branch = "CSE BioTechnology";
     }
 };
+
+
+
 
 int main()
 {
